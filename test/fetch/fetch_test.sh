09766e6d2a8edce19f9bc3bb8811db999031ceb3
Shin'ya Ueoka <ueokande@i-beam.org>
2015-11-17 09:31:07 +0900

    Make fetch idempotent

--- test/fetch/fetch_test.sh
+++ test/fetch/fetch_test.sh
@@ -17,3 +17,9 @@ testcase_fetch_valid_version() {
 testcase_fetch_invalid_version() {
   BASHVM_HOME=$tmpdir assert_false bashvm fetch su.shi
 }
+
+testcase_exit_when_source_already_exist() {
+  mkdir -p $BASHVM_HOME/src/bash-9.9
+  subject bashvm fetch 9.9
+  assert_match 'already exists' "$stdout"
+}
