174c787672e0ffbaf0db8f1ba86730118e8f098c
Shin'ya Ueoka <ueokande@i-beam.org>
2016-12-02 21:17:46 +0900

    Moev shvm-fetch to usr

--- test/fetch/fetch_test.sh
+++ test/fetch/fetch_test.sh
@@ -1,7 +1,7 @@
 source $(dirname $BASH_SOURCE)/../test_helper.sh
 
 testcase_fetch_valid_version() {
-  subject shvm fetch 3.2
+  subject shvm fetch bash-3.2
   assert_true test -d "$SHVM_HOME/src/bash-3.2"
 }
 
@@ -10,12 +10,12 @@ testcase_fetch_invalid_version() {
 }
 
 testcase_exit_when_source_already_exist() {
-  subject shvm fetch X.0
+  subject shvm fetch bash-X.0
   assert_match 'already exists' "$stdout"
 }
 
 testcase_fetch_force() {
   mkdir -p $SHVM_HOME/src/bash-4.0
-  subject shvm fetch --force 4.0
+  subject shvm fetch --force bash-4.0
   assert_true test -f "$SHVM_HOME/src/bash-4.0/configure"
 }
