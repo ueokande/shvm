c1ed1cf731b54edda338d1d06a582d9b64309256
Shin'ya Ueoka <ueokande@i-beam.org>
2016-12-04 22:52:02 +0900

    Add supported shells to list

---
+++ test/list/list_support_test.sh
@@ -0,0 +1,10 @@
+source $(dirname $BASH_SOURCE)/../test_helper.sh
+
+testcase_show_supported_shells() {
+  mkdir $SHVM_HOME/usr/{xsh,ysh,zsh}-support
+  subject shvm list support
+  assert_match 'xsh' "$stdout"
+  assert_match 'ysh' "$stdout"
+  assert_match 'zsh' "$stdout"
+  rmdir $SHVM_HOME/usr/{xsh,ysh,zsh}-support
+}
