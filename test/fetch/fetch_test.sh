b38ee578035e359b2d1539a6ed1ff670996a19e1
Shin'ya Ueoka <ueokande@i-beam.org>
2016-12-05 22:28:06 +0900

    Move fetch for bash-support

--- test/fetch/fetch_test.sh
+++ test/fetch/fetch_test.sh
@@ -1,10 +1,5 @@
 source $(dirname $BASH_SOURCE)/../test_helper.sh
 
-testcase_fetch_valid_version() {
-  subject shvm fetch bash-3.2
-  assert_true test -d "$SHVM_HOME/src/bash-3.2"
-}
-
 testcase_fetch_invalid_version() {
   assert_false shvm fetch su.shi
 }
