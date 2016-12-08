731462d030a92f5d362d3a64451ef159f142b217
Shin'ya Ueoka <ueokande@i-beam.org>
2016-12-09 07:52:33 +0900

    Fix test list

--- test/list/list_local_test.sh
+++ test/list/list_local_test.sh
@@ -9,5 +9,5 @@ testcase_show_list_in_local() {
 
 testcase_show_curren_bash() {
   PATH="$SHVM_HOME/lib/bash-X.1/bin:$PATH" subject shvm list local
-  assert_match '=> bash-X.1' "$stdout"
+  assert_match '\* bash-X.1' "$stdout"
 }
