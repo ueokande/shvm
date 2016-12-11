fffd0f29ddc704faf4c9172b0a56aa1a6f7d2ab7
Shin'ya Ueoka <ueokande@i-beam.org>
2016-12-11 18:44:18 +0900

    Move bin/shvm-init to profile

--- test/main/definition_test.sh
+++ test/main/definition_test.sh
@@ -4,9 +4,3 @@ testcase_shvm_is_function() {
   subject type -t shvm
   assert_match 'function' "$stdout"
 }
-
-testcase_invoke_shvm_init() {
-  subject $(dirname $BASH_SOURCE)/../../bin/shvm-init
-  assert_match 'shvm-init' "$stdout"
-  assert_equal 1 $status
-}
